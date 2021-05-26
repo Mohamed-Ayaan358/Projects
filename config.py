b2b2ce6d2991bd4eca9936a243fd9019149ca611
Mohamed Ayaan <72858215+Mohamed-Ayaan358@users.noreply.github.com>
2021-05-26 09:16:30 +0530

    Delete config.py

--- config.py
+++
@@ -1,4 +0,0 @@
-api_key='GnVEdbt7khqLvMRZVtFYjESZE'
-api_key_secret='6yfEBej2M33cQmtFVYfCW1rhKguDaBccJed4vmjUQO1VLsYuZZ'
-access_token='1330406436409524224-npTMJl5GzOHgd86824OWriUJoUks4c'
-access_token_secret='IL0fZXZwzkVirv4b2cBwuNKejt1yZzXq7ZQ7wL1zowid0'
