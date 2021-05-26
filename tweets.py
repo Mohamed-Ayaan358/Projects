f75b2ddc7b2ec798244aface207a465066b44710
Mohamed Ayaan <72858215+Mohamed-Ayaan358@users.noreply.github.com>
2021-05-26 18:57:15 +0530

    1st

--- tweets.py
+++ tweets.py
@@ -1,14 +1,10 @@
 from textblob import TextBlob
 import tweepy
 import sys
+import config
 
-api_key='GnVEdbt7khqLvMRZVtFYjESZE'
-api_key_secret='6yfEBej2M33cQmtFVYfCW1rhKguDaBccJed4vmjUQO1VLsYuZZ'
-access_token='1330406436409524224-npTMJl5GzOHgd86824OWriUJoUks4c'
-access_token_secret='IL0fZXZwzkVirv4b2cBwuNKejt1yZzXq7ZQ7wL1zowid0'
-
-auth_handler= tweepy.OAuthHandler(consumer_key=api_key,consumer_secret=api_key_secret)
-auth_handler.set_access_token(access_token,access_token_secret)
+auth_handler= tweepy.OAuthHandler(consumer_key=config.api_key,consumer_secret=config.api_key_secret)
+auth_handler.set_access_token(config.access_token,config.access_token_secret)
 
 api=tweepy.API(auth_handler)
 
